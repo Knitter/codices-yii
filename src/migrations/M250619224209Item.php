154c7cd87f63ee51e816f3d1d05cfc1399eca0a6
Sérgio Lopes <knitter.is@gmail.com>
2025-12-16 01:02:42 +0000

    #3 Migration cleanup (stage 1)

--- src/migrations/M250619224209Item.php
+++ src/migrations/M250619224209Item.php
@@ -2,12 +2,6 @@
 
 declare(strict_types=1);
 
-namespace src\Migration;
-
-use Yiisoft\Db\Migration\MigrationBuilder;
-use Yiisoft\Db\Migration\RevertibleMigrationInterface;
-use Yiisoft\Db\Schema\Column\ColumnBuilder;
-
 final class M250619224209Item implements RevertibleMigrationInterface {
     public function up(MigrationBuilder $b): void {
         $b->createTable('item', [
--- src/migrations/M250619224237ItemGenre.php
+++ src/migrations/M250619224237ItemGenre.php
@@ -2,14 +2,7 @@
 
 declare(strict_types=1);
 
-namespace src\Migration;
-
-use Yiisoft\Db\Migration\MigrationBuilder;
-use Yiisoft\Db\Migration\RevertibleMigrationInterface;
-use Yiisoft\Db\Schema\Column\ColumnBuilder;
-
 final class M250619224237ItemGenre implements RevertibleMigrationInterface {
-
     public function up(MigrationBuilder $b): void {
         $b->createTable('item_genre', [
             'itemId' => ColumnBuilder::integer()->notNull(),
